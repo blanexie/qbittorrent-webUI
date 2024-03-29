db967d6db814319696cabd6fb4bbd990dda739b0
xiezhicheng <xiezhicheng@xiaomi.com>
2024-03-29 18:32:07 +0800

    优化pinia

--- src/stores/pinia.ts
+++ src/stores/pinia.ts
@@ -6,74 +6,84 @@ import { GlobalSpeedLimit, Info } from '@/util'
 /**
  * 全局速度相关
  */
-const useTransferInfoStore = defineStore('info', () => {
-    //state
+const useTransferInfoStore =
+  defineStore('info', () => {
+      //state
 
-    const store = ref({
-      intervalId: 0,
-      globalInfo: new Info(),
-      globalSpeedLimit: new GlobalSpeedLimit()
-    })
+      const store = reactive({
+        intervalId: 0,
+        globalInfo: new Info(),
+        globalSpeedLimit: new GlobalSpeedLimit()
+      })
 
-    //get方法
-    const infoState = computed(() => store.value)
-   // const speedLimit = computed(() => store.globalSpeedLimit)
+      //get方法
+      const globalInfo = computed(() => store.globalInfo)
+      const globalSpeedLimit = computed(() => store.globalSpeedLimit)
 
-    // const setIntervalId = (intervalId: number) => {
-    //     clearInterval(globalInfo.intervalId)
-    //     globalInfo.intervalId = intervalId
-    // }
+      const interval = (fun: Function, time: number) => {
+        clearInterval(store.intervalId)
+        store.intervalId = setInterval(fun, time)
+      }
 
-    //actions
-    // const refresh = (infoV: any) => {
-    //     const info = globalInfo.info
-    //     info.dht_nodes = infoV.dht_nodes
-    //     info.dl_info_data.setBytes(infoV.dl_info_data)
-    //     info.dl_info_speed.setBytes(infoV.dl_info_speed)
-    //     info.dl_rate_limit = infoV.dl_rate_limit
-    //     info.up_info_data.setBytes(infoV.up_info_data)
-    //     info.up_info_speed.setBytes(infoV.up_info_speed)
-    //     info.up_rate_limit = infoV.up_rate_limit
-    // }
-    // const setSpeedLimitsMode = (enable: number) => {
-    //     const globalSpeedLimit = globalInfo.globalSpeedLimit
-    //     globalSpeedLimit.speedLimitsMode = enable
-    // }
-    // const setDownloadLimit = (downloadLimit: number) => {
-    //     const globalSpeedLimit = globalInfo.globalSpeedLimit
-    //     globalSpeedLimit.downloadLimit.setBytes(downloadLimit)
-    // }
-    //
-    // const setUploadLimit = (uploadLimit: number) => {
-    //     const globalSpeedLimit = globalInfo.globalSpeedLimit
-    //     globalSpeedLimit.uploadLimit.setBytes(uploadLimit)
-    // }
-    return { infoState  }
-  }
-)
+      //actions
+      const refresh = (infoV: any) => {
+        const info = store.globalInfo
+        info.dht_nodes = infoV.dht_nodes
+        info.dl_info_data.setBytes(infoV.dl_info_data)
+        info.dl_info_speed.setBytes(infoV.dl_info_speed)
+        info.dl_rate_limit = infoV.dl_rate_limit
+        info.up_info_data.setBytes(infoV.up_info_data)
+        info.up_info_speed.setBytes(infoV.up_info_speed)
+        info.up_rate_limit = infoV.up_rate_limit
+      }
+      const setSpeedLimitsMode = (enable: number) => {
+        const globalSpeedLimit = store.globalSpeedLimit
+        globalSpeedLimit.speedLimitsMode = enable
+      }
+      const setDownloadLimit = (downloadLimit: number) => {
+        const globalSpeedLimit = store.globalSpeedLimit
+        globalSpeedLimit.downloadLimit.setBytes(downloadLimit)
+      }
+
+      const setUploadLimit = (uploadLimit: number) => {
+        const globalSpeedLimit = store.globalSpeedLimit
+        globalSpeedLimit.uploadLimit.setBytes(uploadLimit)
+      }
+      return {
+        globalInfo,
+        globalSpeedLimit,
+        interval,
+        refresh,
+        setSpeedLimitsMode,
+        setDownloadLimit,
+        setUploadLimit
+      }
+    }
+  )
 
 /**
  * 登录鉴权的cookie
  */
-const useAuthStore = defineStore('info', () => {
-    //state
-    const cookie = ref('')
-    const login = ref(false)
-    //get方法
-    const cookieState = computed(() => cookie.value)
-    const loginShow = computed(() => login.value)
+const useAuthStore =
+  defineStore('auth', () => {
+      //state
+      const cookie = ref('')
+      const login = ref(false)
+      //get方法
+      const cookieState = computed(() => cookie.value)
+      const loginShow = computed(() => login.value)
 
-    //actions
-    const setCookie = (cookieV: string) => {
-      cookie.value = cookieV
-    }
-    const setLoginShow = (show: boolean) => {
-      login.value = show
-    }
+      //actions
+      const setCookie = (cookieV: string) => {
+        cookie.value = cookieV
+      }
+      const setLoginShow = (show: boolean) => {
+        login.value = show
+      }
 
-    return { loginShow, cookieState, setCookie, setLoginShow }
-  }
-)
+      return { loginShow, cookieState, setCookie, setLoginShow }
+    }
+  )
 
 
 export { useTransferInfoStore, useAuthStore }
